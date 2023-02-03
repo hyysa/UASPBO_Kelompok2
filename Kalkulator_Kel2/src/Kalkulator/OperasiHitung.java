80230ae99a43c1bd470cf2d0aa9c2e8296f1ad71
Hilga Satria P <116480185+hyysa@users.noreply.github.com>
2023-02-04 00:55:19 +0700

    Add files via upload

---
+++ Kalkulator_Kel2/src/Kalkulator/OperasiHitung.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Kalkulator;
+
+/**
+ *
+ * @author WINDOWS 8
+ */
+public interface OperasiHitung {
+    public void Tambah();
+    public void Kurang();
+    public void Kali();
+    public void Bagi();
+    public void Persen();
+}
